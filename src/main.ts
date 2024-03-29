3537162f96aacff4c0307aa56471b38c4e0b8dfc
Attila Szücs <ati.szucs@hotmail.com>
2024-03-30 00:26:56 +0100

    remove forced focus

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,32 @@ import { KeyHandler } from "./KeyHandler";
 import { Keys } from './Keys';
 
 domready(() => {
-    const keyHandler = new KeyHandler();
-    const game = new Game(keyHandler);
+  const keyHandler = new KeyHandler();
+  const game = new Game(keyHandler);
 
-    const canvas = document.getElementById("canvas");
+  const canvas = document.getElementById("canvas");
 
-    canvas.addEventListener("keydown", (event) => {
-        keyHandler.SetKey(event.code, true);
-        if (event.code == Keys.SPACE) {
-            event.preventDefault();
-        }
-    }, false);
+  canvas.addEventListener("keydown", (event) => {
+    keyHandler.SetKey(event.code, true);
+    if (event.code == Keys.SPACE) {
+      event.preventDefault();
+    }
+  }, false);
 
-    canvas.addEventListener("keyup", (event) => {
-        keyHandler.SetKey(event.code, false);
-        if (event.code == Keys.SPACE) {
-            event.preventDefault();
-        }
-    }, false);
+  canvas.addEventListener("keyup", (event) => {
+    keyHandler.SetKey(event.code, false);
+    if (event.code == Keys.SPACE) {
+      event.preventDefault();
+    }
+  }, false);
 
-    document.addEventListener("visibilitychange", () => {
-        if (document.hidden) {
-          game.Pause();
-        } else {
-          game.Play();
-        }
-      });
+  document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+      game.Pause();
+    } else {
+      game.Play();
+    }
+  });
 
-      canvas.focus();
-
-    game.Run();
+  game.Run();
 });

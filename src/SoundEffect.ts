f20160045bc724289654b50ea4a612dbd79604fc
Attila Szücs <ati.szucs@hotmail.com>
2024-03-30 14:47:01 +0100

    hero sfx

--- src/SoundEffect.ts
+++ src/SoundEffect.ts
@@ -1,8 +1,10 @@
 export class SoundEffect {
     private context: AudioContext = new AudioContext();
     private buffer: AudioBuffer;
+    private playing: boolean = false;
+    private source: AudioBufferSourceNode;
 
-    public constructor(path: string) {
+    public constructor(path: string, private allowMultiple: boolean = true) {
         const request = new XMLHttpRequest();
         request.open('GET', path);
         request.responseType = "arraybuffer";
@@ -14,10 +16,25 @@ export class SoundEffect {
         request.send();
     }
 
-    public Play(): void {
-        const source = this.context.createBufferSource();
-        source.buffer = this.buffer;
-        source.connect(this.context.destination);
-        source.start();
+    public Play(playbackRate: number = 1, volume: number = 1): void {
+        if (!this.playing || this.allowMultiple) {
+            const gainNode = this.context.createGain();
+            gainNode.gain.value = volume;
+
+            this.source = this.context.createBufferSource();
+            this.source.buffer = this.buffer;
+            this.source.playbackRate.value = playbackRate;
+            this.source.connect(gainNode).connect(this.context.destination);
+
+            this.playing = true;
+            this.source.onended = () => this.playing = false;
+            this.source.start();
+        }
+    }
+
+    public Stop() {
+        if (this.playing) {
+            this.source.stop();
+        }
     }
 }
